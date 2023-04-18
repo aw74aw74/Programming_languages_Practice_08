a2ba13d3d254465d8f215feabb0f4e06a579aedc
aw74aw74 <aw74@ya.ru>
2023-04-18 18:41:10 +0500

    Задача 54

---
+++ Task_54/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу,
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+using static System.Console;
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i, j] = new Random().Next(min, max + 1);
+    }
+  }
+  return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write($"{inArray[i, j]} ");
+    }
+    WriteLine();
+  }
+}
+
+void SortArray(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      for (int jj = j; jj < arr.GetLength(1); jj++)
+      {
+        if (arr[i, j] < arr[i, jj])
+        {
+          int tmp = arr[i, jj];
+          arr[i,jj] = arr[i,j];
+          arr[i,j] = tmp;
+        }
+      }
+    }
+  }
+}
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine()!);
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine()!);
+int[,] array = GetArray(rows, columns, 1, 10);
+
+WriteLine();
+PrintArray(array);
+SortArray(array);
+WriteLine();
+PrintArray(array);
